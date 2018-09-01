e9df63d859acb9f5eb6264557d0efddcd10d321e
shyamailene <gooinventory@gmail.com>
2018-09-01 12:11:28 +0530

    paypal integration
    paypal integration for donate and commented code for paypal express checkout.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,6 +5,8 @@ import {Register} from "./register";
 import { Observable } from 'rxjs/Rx';
 import { AlertService } from '../_services/index';
 
+declare let paypal: any;
+
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
@@ -45,6 +47,7 @@ export class SignupComponent implements OnInit {
 		this.alertService.success(''+this.message);
 		console.log('result id'+ result.id);
 		console.log('result id'+ JSON.stringify(result));
+		//this.register = result;
         this.isSaving = false;
         //this.activeModal.dismiss(result);
     }
@@ -53,6 +56,63 @@ export class SignupComponent implements OnInit {
 		this.alertService.error('Please try again');
         this.isSaving = false;
     }
+
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+  
+  finalAmount: number = 1;
+
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'Ad_-a7RjjbEI0tTY1rkm1BR2Z-8UZ-ndlRo0wMyw85oiQmVljRHBq3FR_FrE6C_IB7Ww9ezI90ORHxTO',
+      production: '<your-production-key-here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'INR' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+		  console.log(payment);
+		  console.log(data);
+		  alert('test');
+        //Do something when payment is successful.
+      }).catch(function(err) {
+		  console.error('execute error:', err);
+	  });
+    },
+	onCancel: (data, actions) => {
+          console.log('OnCancel'+JSON.stringify(data));
+        },
+    onError: (err) => {
+          console.log('OnError');
+        },  };
+
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        //paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  }
 
 	
 }
